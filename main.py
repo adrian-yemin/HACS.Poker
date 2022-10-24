b58d14c916a13f7cac2e8f72413c70c3ad691ad8
adrian-yemin <adrian_yemin@student.waylandps.org>
2022-10-24 11:33:02 -0400

    Set up Background

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from pygame.locals import (
     QUIT,
 )
 import random
+
 pygame.mixer.init()
 pygame.init()
 
@@ -18,4 +19,54 @@ SCREEN_HEIGHT = 600
 screen = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
 
 
-# class Card(pygame.sprite.sprite)
+class Card(pygame.sprite.Sprite):
+    def __init__(self):
+        super(Card, self).__init__()
+        self.surf = pygame.Surface((75, 25))
+        self.surf.fill((255, 255, 255))
+        self.rect = self.surf.get_rect()
+
+
+running = True
+from pygame.locals import (
+    RLEACCEL,
+    K_UP,
+    K_DOWN,
+    K_LEFT,
+    K_RIGHT,
+    K_ESCAPE,
+    KEYDOWN,
+    QUIT,
+)
+import random
+
+pygame.mixer.init()
+pygame.init()
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+screen = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
+
+
+class Card(pygame.sprite.Sprite):
+    def __init__(self):
+        super(Card, self).__init__()
+        self.screen = screen
+        self.card= pygame.draw.rect(screen, (255, 255, 255), [780, 250, 10, 100])
+
+
+running = True
+while running:
+
+    # Did the user click the window close button?
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill((34, 139, 34))
+
+    pygame.display.flip()
+
+while running:
+
+    pygame.quit()
