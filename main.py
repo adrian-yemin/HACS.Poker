7113fa870669afdc2addce130b13439fe068ec58
adrian-yemin <adrian_yemin@student.waylandps.org>
2022-11-01 09:01:35 -0400

    Instantiate all 52 cards

--- main.py
+++ main.py
@@ -19,18 +19,31 @@ SCREEN_HEIGHT = 600
 screen = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
 
 
-class Card(pygame.sprite.Sprite):
-    def __init__(self):
-        super(Card, self).__init__()
-        self.surf = pygame.Surface((75, 25))
-        self.surf.fill((255, 255, 255))
-        self.rect = self.surf.get_rect()
+class Card:
 
+    def __init__(self, value, suit, image):
+        self.value = value
+        self.suit = suit
+        self.image = image
+
+    def printSelf(self):
+        print(self.value, self.suit, self.image)
+
+
+suit = ['h', 's', 'c', 'd']
+
+deck = []
+for c in range(len(suit)):
+    color = suit[c]
+    for value in range(1, 14):
+        deck.append(Card(value, color, str(value) + str(color) + '.png'))
+
+for x in range(len(deck)):
+    print(deck[x].printSelf())
 
 running = True
 while running:
 
-    # Did the user click the window close button?
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
